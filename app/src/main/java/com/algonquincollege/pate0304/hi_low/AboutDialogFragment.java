279b4fd25e083a0aa4bf8d287ac010b0dade37cf
Jay Patel <pate0304@algonquinlive.com>
2016-10-02 23:14:04 -0400

    Added Image and About

---
+++ app/src/main/java/com/algonquincollege/pate0304/hi_low/AboutDialogFragment.java
@@ -0,0 +1,34 @@
+package com.algonquincollege.pate0304.hi_low;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDialogFragment;
+
+/**
+ * Created by shiva on 2016-10-02.
+ */
+
+public class AboutDialogFragment extends AppCompatDialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        //TODO pro-tip: cascading messages
+        builder.setTitle(R.string.about)
+                //TODO replace with your name + userID
+                .setMessage(R.string.author)
+                .setCancelable(false)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
